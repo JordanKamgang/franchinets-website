ac509fa2b1c7455fc58b05983187e491ff2decf4
JordanKamgang <kamgangjordan@yahoo.fr>
2023-09-24 23:10:36 +0200

    prepare some components for the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooPageComponent } from './components/foo-page/foo-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooPageComponent,
+    HomePageComponent,
+    MenuBarComponent,
+    PageHeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/foo-page/foo-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'franchinets-foo-page',
+  templateUrl: './foo-page.component.html',
+  styleUrls: ['./foo-page.component.scss']
+})
+export class FooPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
